e877df1327bdcaef94bed4970d8b03cfacb2b728
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:29:08 +0530

    Update 2.22.py

--- Tutorial_2/2.22.py
+++ Tutorial_2/2.22.py
@@ -1,12 +1,12 @@
-def filter_words(sentence, words_to_exclude):
-    word_list = sentence.split()  
-    filtered_list = [word for word in word_list if word not in words_to_exclude]
+def rem_words(texts, words_removed):
+    word_list = texts.split()  
+    filtered_list = [word for word in word_list if word not in words_removed]
     return " ".join(filtered_list) 
 
 user_input = input("Enter a string: ")
 
-excluded_words = input("Enter words to remove (separated by spaces): ").split()
+words_removed = input("Enter words to remove (separated by spaces): ").split()
 
-result_text = filter_words(user_input, excluded_words)
+result_text = rem_words(user_input, words_removed)
 print("\nString after removing given words:")
 print(result_text)
