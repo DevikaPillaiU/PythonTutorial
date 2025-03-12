d0b6033eb10f37ed29285a49cd7b051f6b7af873
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:35:30 +0530

    Update 2.29.py

--- Tutorial_2/2.29.py
+++ Tutorial_2/2.29.py
@@ -1,20 +1,18 @@
-"""Write a program to do basic set operations"""
-
-def set_operations(set1, set2):
-    union_set = set1 | set2
-    intersection_set = set1 & set2
-    difference_set1 = set1 - set2
-    difference_set2 = set2 - set1
-    symmetric_difference_set = set1 ^ set2
-    return union_set, intersection_set, difference_set1, difference_set2, symmetric_difference_set
+def set_ops(set1, set2):
+    union_res = set1 | set2
+    intersection_res = set1 & set2
+    diff_res1 = set1 - set2
+    diff_res2 = set2 - set1
+    symmetric_diff_res = set1 ^ set2
+    return union_res, intersection_res, diff_res1, diff_res2, symmetric_diff_res
 
 set1 = set(map(int, input("Enter elements of first set (space-separated): ").split()))
 set2 = set(map(int, input("Enter elements of second set (space-separated): ").split()))
 
-union_set, intersection_set, difference_set1, difference_set2, symmetric_difference_set = set_operations(set1, set2)
+union_res, intersection_res, diff_res1, diff_res2, symmetric_diff_res = set_ops(set1, set2)
 
-print(f"\nUnion: {union_set}")
-print(f"Intersection: {intersection_set}")
-print(f"Difference (Set1 - Set2): {difference_set1}")
-print(f"Difference (Set2 - Set1): {difference_set2}")
-print(f"Symmetric Difference: {symmetric_difference_set}")
+print(f"\nUnion: {union_res}")
+print(f"Intersection: {intersection_res}")
+print(f"Difference (Set1 - Set2): {diff_res1}")
+print(f"Difference (Set2 - Set1): {diff_res2}")
+print(f"Symmetric Difference: {symmetric_diff_res}")
