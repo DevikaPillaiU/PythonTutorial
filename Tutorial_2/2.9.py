feabef1c0b19df9d1ef53d362c7ebc561480225c
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:40:40 +0530

    Update 2.9.py

--- Tutorial_2/2.9.py
+++ Tutorial_2/2.9.py
@@ -1,11 +1,9 @@
-#Write a Program to reverse the first and second half of a string separately
+text = input("Enter the string: ")
 
-s=input("Enter the string : ")
+length = len(text)
+middle = length // 2
 
-n=len(s)
-mid=n//2
-
-for i in range(mid-1,-1,-1):
-    print(s[i], end="")
-for i in range(n-1,mid-1,-1):
-    print(s[i], end="")
+for i in range(middle - 1, -1, -1):
+    print(text[i], end="")
+for i in range(length - 1, middle - 1, -1):
+    print(text[i], end="")
