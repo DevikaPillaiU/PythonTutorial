e1d88a20bc9270ee9f57153e6d2b2a316f742ac6
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 22:19:05 +0530

    Update 1.22.py

--- Tutorial_1/1.22.py
+++ Tutorial_1/1.22.py
@@ -1,6 +1,11 @@
-year = int(input("Enter a year: "))
+user_year = int(input("Enter a year: "))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"{year} is a Leap Year.")
-else:
-    print(f"{year} is NOT a Leap Year.")
+def determine_leap_year(year_input):
+    if (year_input % 4 == 0 and year_input % 100 != 0) or (year_input % 400 == 0):
+        return f"{year_input} is a Leap Year."
+    else:
+        return f"{year_input} is NOT a Leap Year."
+
+output = determine_leap_year(user_year)
+print(output)
+
