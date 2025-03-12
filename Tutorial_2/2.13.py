3e939ef251323bd4055d60208bc0b0ad8aa25467
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:20:21 +0530

    Update 2.13.py

--- Tutorial_2/2.13.py
+++ Tutorial_2/2.13.py
@@ -1,17 +1,13 @@
-""" Write a python function to find the area of a circle."""
-
-
 import math
 
-def area_of_circle(radius):
-    if radius < 0:
+def calc_circle_area(r):
+    if r < 0:
         return "Radius cannot be negative"
     
-    area = math.pi * radius ** 2
+    area = math.pi * r ** 2
     return area
 
 
-r = float(input("Enter the radius of the circle: "))
+radius_input = float(input("Enter the radius of the circle: "))
 
-
-print(f"Area of the circle: {area_of_circle(r):.2f}")
+print(f"Area of the circle: {calc_circle_area(radius_input):.2f}")
