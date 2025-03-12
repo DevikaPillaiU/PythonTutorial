004153738ddcd1f7d9a239a3f58fc8602908d917
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:01:52 +0530

    Update 1.15.py

--- Tutorial_1/1.15.py
+++ Tutorial_1/1.15.py
@@ -1,11 +1,12 @@
-def is_prime(n):
-    if n < 2:
+
+def check_prime(num):
+    if num < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1): 
-        if n % i == 0:
+    for i in range(2, int(num ** 0.5) + 1): 
+        if num % i == 0:
             return False
     return True
 
-for num in range(2, 1000):
-    if is_prime(num):
-        print(num, end=" ")
+for number in range(2, 1000):
+    if check_prime(number):
+        print(number, end=" ")
