a7ca6b512b96727d4401d8b21345dce08fbee6e7
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:16:17 +0530

    Update 2.12.py

--- Tutorial_2/2.12.py
+++ Tutorial_2/2.12.py
@@ -1,10 +1,3 @@
-"""Write Python script for converting Binary number into decimal number."""
-
-
-binary = input("Enter a binary number: ")
-
-
-decimal = int(binary, 2)
-
-
-print(f"Decimal representation: {decimal}")
+bin_num = input("Enter a binary number: ")
+dec_num = int(bin_num, 2)
+print(f"Decimal representation: {dec_num}")
