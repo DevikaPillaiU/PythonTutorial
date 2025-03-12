f09c9855393171bcf9749a29f33c2bbf5f0b9e05
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:48:15 +0530

    Update 1.6.py

--- Tutorial_1/1.6.py
+++ Tutorial_1/1.6.py
@@ -1,16 +1,16 @@
-def is_right_triangle(a, b, c):
-    sides = sorted([a, b, c])
+def rt_tri(s1, s2, s3):
+    sides = sorted([s1, s2, s3])
     
     if round(sides[0]**2 + sides[1]**2, 5) == round(sides[2]**2, 5):
         return "Yes, it is a right-angled triangle."
     else:
         return "No, it is not a right-angled triangle."
 
-a = float(input("Enter first side: "))
-b = float(input("Enter second side: "))
-c = float(input("Enter third side: "))
+s1 = float(input("Enter first side: "))
+s2 = float(input("Enter second side: "))
+s3 = float(input("Enter third side: "))
 
-if a + b > c and a + c > b and b + c > a:
-    print(is_right_triangle(a, b, c))
+if s1 + s2 > s3 and s1 + s3 > s2 and s2 + s3 > s1:
+    print(rt_tri(s1, s2, s3))
 else:
     print("Invalid triangle. The sum of any two sides must be greater than the third side.")
