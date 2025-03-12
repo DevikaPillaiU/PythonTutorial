41e45d9e745acef5dc8b138645f88314cd5aa8b9
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 21:40:07 +0530

    Update 2.8.py

--- Tutorial_2/2.8.py
+++ Tutorial_2/2.8.py
@@ -1,7 +1,7 @@
-s= input("Enter the string")
+string= input("Enter the string")
 ss1= input("Enter the substring ")
 ss2= input("Enter the new substring ")
 
-if ss1 in s:
-    s = s.replace(ss1,ss2)
-    print(s)
+if ss1 in string:
+    string = s.replace(ss1,ss2)
+    print(string)
