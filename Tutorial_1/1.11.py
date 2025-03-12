90d0e12d1936df2b8951877dc7c967b7658ec038
DevikaPillaiU <157577742+DevikaPillaiU@users.noreply.github.com>
2025-03-12 20:52:26 +0530

    Update 1.11.py

--- Tutorial_1/1.11.py
+++ Tutorial_1/1.11.py
@@ -1,14 +1,15 @@
-def sum_of_even_cubes(n):
+
+def sum_of_even_powers(limit):
     total = 0 
 
-    for i in range(2, n + 1, 2): 
+    for i in range(2, limit + 1, 2): 
         total += i ** 3 
 
     return total
 
-n = int(input("Enter a positive integer: "))
+num = int(input("Enter a positive integer: "))
 
-if n <= 0:
+if num <= 0:
     print("Please enter a positive integer.")
 else:
-    print(f"Sum of cubes of even numbers up to {n}: {sum_of_even_cubes(n)}")
+    print(f"Sum of cubes of even numbers up to {num}: {sum_of_even_powers(num)}")
